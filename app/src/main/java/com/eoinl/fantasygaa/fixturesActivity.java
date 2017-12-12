4a1758aa610f0acd69dbe105276c878114bfc0fa
EoinLar <32529286+EoinLar@users.noreply.github.com>
2017-12-12 02:29:48 +0000

    UserTeamActivity changes: Player selection buttons now opening a menu based on their position(name).  - Pop up menu with 4 basic choices.

--- app/src/main/java/com/eoinl/fantasygaa/fixturesActivity.java
+++ app/src/main/java/com/eoinl/fantasygaa/fixturesActivity.java
@@ -4,11 +4,15 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -17,6 +21,8 @@ public class fixturesActivity extends AppCompatActivity {
 
     String url = "https://www.rte.ie/sport/results/gaa/2018/6120/fixtures/";
     ProgressDialog mProgressDialog;
+    //String fixtureText;
+    Spanned fixtureText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +43,13 @@ public class fixturesActivity extends AppCompatActivity {
     // Title AsyncTask
     private class fixtures extends AsyncTask<Void, Void, Void> {
         String title, date;
+        StringBuilder fixtures = new StringBuilder();
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             mProgressDialog = new ProgressDialog(fixturesActivity.this);
-            mProgressDialog.setTitle("Android Basic JSoup Tutorial");
+            mProgressDialog.setTitle("Loading Fixtures");
             mProgressDialog.setMessage("Loading...");
             mProgressDialog.setIndeterminate(false);
             mProgressDialog.show();
@@ -54,9 +61,12 @@ public class fixturesActivity extends AppCompatActivity {
                 // Connect to the web site
                 Document doc = Jsoup.connect(url).get();
                 // Get the html document title
-                title = doc.title();
-                Elements dates = doc.getAllElements();
-                date = dates.html();
+                //title = doc.title();
+                Elements fixture = doc.getElementsByClass("results-widget soccer-results gaa-results");
+                fixtureText = Html.fromHtml(fixture.toString());
+                //Elements dates = doc.getAllElements();
+                //dates.hasClass("date");
+                //date = dates.html();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -67,8 +77,10 @@ public class fixturesActivity extends AppCompatActivity {
         protected void onPostExecute(Void result) {
             // Set title into TextView
             TextView fixturesTextView = (TextView) findViewById(R.id.tv);
+            fixturesTextView.setMovementMethod(new ScrollingMovementMethod());
             //fixturesTextView.setText(title);
-            fixturesTextView.setText(date);
+            //fixturesTextView.setText(title);
+            fixturesTextView.append("\n\n" + fixtureText);
             mProgressDialog.dismiss();
         }
     }
--- app/src/main/java/com/eoinl/fantasygaa/userTeamActivity.java
+++ app/src/main/java/com/eoinl/fantasygaa/userTeamActivity.java
@@ -1,12 +1,15 @@
 package com.eoinl.fantasygaa;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class userTeamActivity extends AppCompatActivity {
+public class userTeamActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +21,153 @@ public class userTeamActivity extends AppCompatActivity {
 
         TextView tvTeamName = (TextView) findViewById(R.id.userTeamName);
         tvTeamName.setText(userEmail);
+
+        Button gkBtn = (Button) findViewById(R.id.btnGK);
+        gkBtn.setOnClickListener(this);
+        Button lfbBtn = (Button) findViewById(R.id.btnLFB);
+        lfbBtn.setOnClickListener(this);
+        Button cfbBtn = (Button) findViewById(R.id.btnCFB);
+        cfbBtn.setOnClickListener(this);
+        Button rfbBtn = (Button) findViewById(R.id.btnRFB);
+        rfbBtn.setOnClickListener(this);
+        Button lhbBtn = (Button) findViewById(R.id.btnLHB);
+        lhbBtn.setOnClickListener(this);
+        Button chbBtn = (Button) findViewById(R.id.btnCHB);
+        chbBtn.setOnClickListener(this);
+        Button rhbBtn = (Button) findViewById(R.id.btnRHB);
+        rhbBtn.setOnClickListener(this);
+        Button lmBtn = (Button) findViewById(R.id.btnLM);
+        lmBtn.setOnClickListener(this);
+        Button rmBtn = (Button) findViewById(R.id.btnRM);
+        rmBtn.setOnClickListener(this);
+        Button lhfBtn = (Button) findViewById(R.id.btnLHF);
+        lhfBtn.setOnClickListener(this);
+        Button chfBtn = (Button) findViewById(R.id.btnCHF);
+        chfBtn.setOnClickListener(this);
+        Button rhfBtn = (Button) findViewById(R.id.btnRHF);
+        rhfBtn.setOnClickListener(this);
+        Button lffBtn = (Button) findViewById(R.id.btnLFF);
+        lffBtn.setOnClickListener(this);
+        Button cffBtn = (Button) findViewById(R.id.btnCFF);
+        cffBtn.setOnClickListener(this);
+        Button rffBtn = (Button) findViewById(R.id.btnRFF);
+        rffBtn.setOnClickListener(this);
     }
 
-    public void selectPlayer(View v) {
+    //implement the onClick method here
+    public void onClick(View v) {
+        // Perform action on click
+        switch(v.getId()) {
+            case R.id.btnGK:
+                //Open Goalkeeper menu, position 1
+                selectPlayer(1);
+                break;
+            case R.id.btnLFB:
+            case R.id.btnCFB:
+            case R.id.btnRFB:
+                //Open Full Back menu, position 2
+                selectPlayer(2);
+                break;
+            case R.id.btnLHB:
+            case R.id.btnCHB:
+            case R.id.btnRHB:
+                //Open Half Back menu, position 3
+                selectPlayer(3);
+                break;
+            case R.id.btnLM:
+            case R.id.btnRM:
+                //Open Midfield menu, position 4
+                selectPlayer(4);
+                break;
+            case R.id.btnLHF:
+            case R.id.btnCHF:
+            case R.id.btnRHF:
+                //Open Half Forward menu, position 5
+                selectPlayer(5);
+                break;
+            case R.id.btnLFF:
+            case R.id.btnCFF:
+            case R.id.btnRFF:
+                //Open Full Forward menu, position 6
+                selectPlayer(6);
+                break;
+            default:
+                break;
+        }
+    }
 
+    public void selectPlayer(int position) {
+
+        CharSequence colors[] = new CharSequence[] {"Number 1", "Number 2", "Number 3", "Number 4"};
+        AlertDialog.Builder builder;
+
+        switch (position) {
+            case 1:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Goalkeeper Pick a color");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                    }
+                });
+                builder.show();
+                break;
+            case 2:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Full Backs Pick a color");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                    }
+                });
+                builder.show();
+                break;
+            case 3:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Half Backs Pick a color");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                    }
+                });
+                builder.show();
+                break;
+            case 4:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Midfield Pick a color");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                    }
+                });
+                builder.show();
+                break;
+            case 5:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Half Forward Pick a color");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                    }
+                });
+                builder.show();
+                break;
+            case 6:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Full Forward Pick a color");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+                    }
+                });
+                builder.show();
+                break;
+        }
     }
 }
